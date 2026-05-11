79f9d7d2255748a7361adc4ef805a62da171f969
Joao Pedro <jjoaoxz20@gmail.com>
2026-05-11 10:35:29 -0300

    add: SecurityConfiguration -> autorizeHttpRequests only POST at /auth/**

--- src/main/java/com/dallagnoldev/gymguy/repository/IUserRepository.java
+++ src/main/java/com/dallagnoldev/gymguy/repository/IUserRepository.java
@@ -1,11 +1,9 @@
 package com.dallagnoldev.gymguy.repository;
 
 import com.dallagnoldev.gymguy.model.UserEntity;
-import org.jspecify.annotations.NonNull;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.lang.ScopedValue;
 import java.util.Optional;
 
 @Repository
